852e9cb378ae9729bd3a65d558d8370b825d0fbf
G284802022 <228480@st.takukshoku-u.ac.jp>
2023-06-22 16:33:12 +0900

    6/22

--- kazuate.js
+++ kazuate.js
@@ -12,13 +12,26 @@ hantei();
 hantei();
 hantei();
 hantei();
+hantei();
+hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  
+  kaisu = kaisu + 1;
+  console.log("1回目の予想: " + yoso);
   // 課題3-1: 正解判定する
+  if (yoso === kotae){
+    console.log("正解です．おめでとう!");
+  }else if (kaisu > 4){
+    console.log("答えは " + kotae + " でした．すでにゲームは終わっています");
+  }
+  else if (yoso < kotae){
+    console.log("まちがい．答えはもっと大きいですよ");
+  }else if (yoso > kotae){
+    console.log("まちがい．答えはもっと小さいですよ");
+  }
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
 }
